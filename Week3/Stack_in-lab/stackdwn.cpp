c823bf7c4a245a5fd9affafae12b0ddf4117eab9
KANG HEE LEE <67950809+LeeKangh22@users.noreply.github.com>
2020-11-15 18:06:22 +0900

    Week3 Stack

---
+++ Week3/Stack_in-lab/stackdwn.cpp
@@ -0,0 +1,69 @@
+#include"stackarr.h"
+#include<iostream>
+
+using namespace std;
+
+// Constructor
+
+template < class DT >
+Stack<DT>::Stack(int maxNumber) : maxSize(maxNumber), top(-1)
+{
+	element = new DT[maxSize];
+}
+
+// Destructor
+template < class DT >
+Stack<DT>::~Stack() {
+	delete[]element;
+}
+
+// Stack manipulation operations
+template < class DT >
+void Stack<DT>::push(const DT& newElement) {
+	if (full() == 1) {
+		cout << "Stack is Full" << endl;
+	}
+	else {
+		++top;
+		element[maxSize - top] = newElement;
+	}
+}
+//Push element
+
+template < class DT >
+DT Stack<DT>::pop() {
+	if (empty()) {
+		cout << "Stack is Empty" << endl;
+	}
+	else {
+		top--;
+		return element[maxSize - top - 1];
+	}
+
+}
+// Pop element
+template < class DT >
+void Stack<DT>::clear() {
+	top = -1;
+}                    // Clear stack
+
+// Stack status operations
+template < class DT >
+bool Stack<DT>::empty() const {
+	if (top == -1) return 1;
+	else return 0;
+};                    // Stack is empty
+template < class DT >
+bool Stack<DT>::full() const {
+	if (top == maxSize - 1) return 1;
+	else return 0;
+}
+// Stack is full
+
+// Output the stack structure -- used in testing/debugging
+template < class DT >
+void Stack<DT>::showStructure() const {
+	for (int i = maxSize - top; i <= maxSize; i++)
+		cout << "[" << element[i] << "]";
+}
+//made by 2017110274 ÀÌ°­Èñ
